0d107a538624a6419cd98f1669c28963cd0ec2fa
MAINMAPC\02rak <0>
2026-08-08 10:51:18 +0900

    Clamp,max,minの追加

--- src/Common/Math/MathF.cs
+++ src/Common/Math/MathF.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace RaruLib
@@ -10,6 +11,23 @@ namespace RaruLib
         public static bool Step(float x, float a)
         {
             return a < x ? true : false;
+        }
+
+        public static float Clamp(float value, float min,float max)
+        {
+            if (min > max)
+                throw new ArgumentException("min must be less than or equal to max.");
+            return Min(Max(value, min), max);
+        }
+
+        public static float Min(float a, float b)
+        {
+            return (a > b) ? b : a;
+        }
+
+        public static float Max(float a, float b)
+        {
+            return (a > b) ? a : b;
         }
     }
 }
